6e87a3cc3c18be83cec4ff12ecd5757f5b89fd0a
adityjha0 <aditya2005aps@gmail.com>
2023-12-20 16:20:13 +0530

    initialize a window

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 int main(){
 
     InitWindow(1600,1000,"PONG");
-    
-
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(WHITE);
+        EndDrawing();
+    }
+    CloseWindow();
 
     return 0;
 }
