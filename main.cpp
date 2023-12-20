c4c5de0f42b8e9db487b4771cb69ca491be638a1
adityjha0 <aditya2005aps@gmail.com>
2023-12-20 18:50:13 +0530

    basic layout

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include "raylib.h"
 
+const int width = 1600, height = 1000;
+
 int main(){
 
-    InitWindow(1600,1000,"PONG");
+    InitWindow(width,height,"PONG");
+
+
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE);
         DrawRectangle(10,10,1580,980,BLACK);
+        DrawLine(800,10,800,990,WHITE);
+        DrawRectangle(1550,350,15,300,WHITE);
+        DrawRectangle(35,350,15,300,WHITE);
         EndDrawing();
     }
     CloseWindow();
