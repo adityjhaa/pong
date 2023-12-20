473ef94f6701d8444d75570b6aab30e9c913dec3
adityjha0 <aditya2005aps@gmail.com>
2023-12-20 18:52:22 +0530

    basic layout

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(){
         ClearBackground(WHITE);
         DrawRectangle(10,10,1580,980,BLACK);
         DrawLine(800,10,800,990,WHITE);
+        DrawCircle(800,500,20.f,WHITE);
         DrawRectangle(1550,350,15,300,WHITE);
         DrawRectangle(35,350,15,300,WHITE);
         EndDrawing();
