960585e902f25b312b53341aa23e55b08b76e2c9
adityjha0 <aditya2005aps@gmail.com>
2023-12-22 16:50:32 +0530

    basic ai

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main(){
     float cir_vel_y = 500.f;
     
 
-    SetTargetFPS(60);
+    SetTargetFPS(360);
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE);
@@ -31,7 +31,7 @@ int main(){
         DrawLine(800,10,800,990,WHITE);
         DrawCircle(circle_x,circle_y,circle_rad,WHITE);
 
-        float player_vel{};
+        float player_vel{},ai_velocity{};
         if(IsKeyDown(KEY_DOWN) and player.posy<=675){
             player.posy+=velocity*dt;
             player_vel=velocity;
@@ -44,13 +44,25 @@ int main(){
         if(circle_y<=30.f){cir_vel_y*=-1.f;}
         if(circle_y>=970.f){cir_vel_y*=-1.f;}
 
+        if(ai.posy+150>circle_y and ai.posy>=25 and cir_vel_x<0){
+            ai.posy-=velocity*dt;
+            ai_velocity=-velocity;
+        }
+        if(ai.posy+150<circle_y and ai.posy<=675 and cir_vel_x<0){
+            ai.posy+=velocity*dt;
+            ai_velocity=velocity;
+        }
+
         if(CheckCollisionCircleRec(Vector2{circle_x,circle_y},circle_rad,
         Rectangle{player.posx,player.posy,player.width,player.height})){
             cir_vel_x*=-1.f;
             cir_vel_y+=player_vel;
         }
         if(CheckCollisionCircleRec(Vector2{circle_x,circle_y},circle_rad,
-        Rectangle{ai.posx,ai.posy,ai.width,ai.height})){cir_vel_x*=-1.f;}
+        Rectangle{ai.posx,ai.posy,ai.width,ai.height})){
+            cir_vel_x*=-1.f;
+            cir_vel_y+=ai_velocity;
+        }
 
         circle_x += cir_vel_x*dt;
         circle_y += cir_vel_y*dt;
